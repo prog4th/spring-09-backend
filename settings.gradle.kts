2b4024709aab989ba001e8ba782cc8cc9fa33e7c
prog4th <prog4th@proton.me>
2026-01-13 10:31:25 +0900

    🎉프로젝트 시작

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "spring-09-backend"
---
+++ src/test/kotlin/kr/ac/kumoh/s20260000/spring09backend/Spring09BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20260000.spring09backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring09BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
